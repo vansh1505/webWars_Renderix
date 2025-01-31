b52e267ab58e7667e9b74970f58d83ef51c8f753
vansh1505 <vanshsharma.vs321@gmail.com>
2025-01-31 19:04:04 +0530

    Initialize Vite + React project with Tailwind CSS setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen text-3xl font-semibold bg-black text-white'>
+      <p>Welcome to Web Wars 2025</p>
+      <p className='text-gray-400'>Team Renderix</p>
+    </div>
+  )
+}
+
+export default App
